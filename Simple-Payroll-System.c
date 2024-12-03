a779f38e8fbf8edee5375bdb8800a267a02efe3a
PRINCE <princepatel15306@gmail.com>
2024-12-03 18:56:18 +0530

    Update Simple-Payroll-System.c

--- Simple-Payroll-System.c
+++ Simple-Payroll-System.c
@@ -11,9 +11,10 @@ struct Employee
  float gross_salary;
 };
 
-float calculateGrossSalary(float basic, float allowances, float deductions) {
- return basic + allowances - deductions;
-}
+float calculateGrossSalary(float basic, float allowances, float deductions) 
+ {
+    return basic + allowances - deductions;
+ }
 
 void displayEmployee(struct Employee emp)
 {
@@ -24,7 +25,10 @@ printf("\nAllowances: %.2f", emp.allowances);
  printf("\nDeductions: %.2f", emp.deductions);
  printf("\nGross Salary: %.2f\n", emp.gross_salary);
 }
-int main () {
+
+
+int main () 
+{
  int n, i;
 printf("Enter the number of employees: ");
 scanf("%d", &n);
@@ -33,27 +37,27 @@ struct Employee *employees = (struct Employee *)malloc(n * sizeof(struct Employe
 
 for (i = 0; i < n; i++)
  {
- printf("\nEnter details for Employee %d:\n", i + 1);
-printf("ID: ");
-scanf("%d", &employees[i].id);
-printf("Name: ");
-scanf(" %[^\n]", employees[i].name); 
-printf("Basic Salary: ");
- scanf("%f", &employees[i].basic_salary);
-printf("Allowances: ");
-scanf("%f", &employees[i].allowances);
-printf("Deductions: ");
-scanf("%f", &employees[i].deductions);
+   printf("\nEnter details for Employee %d:\n", i + 1);
+   printf("ID: ");
+   scanf("%d", &employees[i].id);
+   printf("Name: ");
+   scanf(" %[^\n]", employees[i].name); 
+   printf("Basic Salary: ");
+   scanf("%f", &employees[i].basic_salary);
+   printf("Allowances: ");
+   scanf("%f", &employees[i].allowances);
+   printf("Deductions: ");
+   scanf("%f", &employees[i].deductions);
 
-employees[i].gross_salary = calculateGrossSalary(employees[i].basic_salary,employees[i].allowances,employees[i].deductions);
-}
+   employees[i].gross_salary = calculateGrossSalary(employees[i].basic_salary,employees[i].allowances,employees[i].deductions);
+ }
 
-printf("\nEmployee Payroll Details:\n");
-for (i = 0; i < n; i++)
- {
-displayEmployee(employees[i]);
-}
+   printf("\nEmployee Payroll Details:\n");
+   for (i = 0; i < n; i++)
+      {
+        displayEmployee(employees[i]);
+      }
 
-free(employees);
-return 0;
+   free(employees);
+   return 0;
 }
